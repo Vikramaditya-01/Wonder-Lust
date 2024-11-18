b9e39dc29a97f349fd2518637403ef7f58d821bf
Vikramaditya <vikramadityagorai@gmail.com>
2024-11-19 04:24:06 +0530

    CRUD compleat

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 const listing = require('./models/listing.js');
 const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
-// Set view engine and views directory
+// Set & use engines and directorys
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+
 
 // Database Connection
 const MONGOURL = "mongodb://127.0.0.1:27017/Wonderlust";
@@ -16,7 +23,9 @@ async function main() {
     await mongoose.connect(MONGOURL);
 }
 
-// Routes
+///////// Routes////
+
+// Root route
 app.get('/', (req, res) => {
     res.send('Hii I am root');
 });
@@ -32,7 +41,46 @@ app.get('/listings', async (req, res) => {
     }
 });
 
+// New route
+app.get('/listings/new', (req, res) => {
+    res.render('listings/new');
+});
+
 // Show route
+
+app.get('/listings/:id', async (req, res) => {
+    let { id } = req.params;
+    const listings = await listing.findById(id);
+    res.render('listings/show', { listings });
+});
+
+// Create route
+app.post('/listings', async (req, res) => {;
+const Newlisting = new listing(req.body.listing);
+await Newlisting.save();
+res.redirect(`/listings`);
+});
+
+// Edit route
+app.get('/listings/:id/edit', async (req, res) => {
+    let { id } = req.params;
+    const listings = await listing.findById(id);
+    res.render('listings/edit', { listings });
+});
+
+// Update route
+app.put('/listings/:id', async (req, res) => {
+    let { id } = req.params;
+    await listing.findByIdAndUpdate(id , { ...req.body.listing });
+    res.redirect(`/listings`);
+});
+
+// Delete route
+app.delete('/listings/:id', async (req, res) => {
+    let { id } = req.params;
+    await listing.findByIdAndDelete(id);
+    res.redirect('/listings');
+});
 
 
 // Server listening
