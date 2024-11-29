c5107e2efb4d4d1b3d7d9ab68a1f1c0025d6c70c
Vikramaditya <vikramadityagorai@gmail.com>
2024-11-30 01:32:20 +0530

    resolving an image issue

--- models/listing.js
+++ models/listing.js
@@ -11,15 +11,14 @@ const listingSchema = new Schema({
         required: true
     },
     image: {
-        filename: {
-            type: String,
-            default: 'defaultImage'
-        },
-        url: {
-            type: String,
-            default: "https://unsplash.com/photos/a-large-castle-like-building-with-a-clock-tower-nsXv1nWfQ2c"
-        }
-    },
+        type: String,
+        default:
+          "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v) =>
+          v === ""
+            ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            : v,
+      },
     price : {
         type: Number,
         required: true
