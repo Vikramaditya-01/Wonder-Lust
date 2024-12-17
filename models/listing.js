aa4e45e97f09c43a27b9aaefea64da3b6977d01c
Vikramaditya <vikramadityagorai@gmail.com>
2024-12-18 04:00:46 +0530

    add review schema

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const review = require('./review');
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -31,6 +32,12 @@ const listingSchema = new Schema({
         type: String,
         required: true
     },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 const Listing = mongoose.model('Listing', listingSchema);  // Listing is the name of the model
