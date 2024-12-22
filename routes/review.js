52d7e5d9c5bc29f9c059dd0b768ceda6e7580b66
Vikramaditya <vikramadityagorai@gmail.com>
2024-12-22 08:53:26 +0530

    Authorization for new rewiew

--- routes/review.js
+++ routes/review.js
@@ -3,12 +3,13 @@ const router = express.Router({ mergeParams: true });
 const listing = require('../models/listing.js');
 const wrapAsync = require('../utils/wrapAsync');
 const Review = require('../models/review.js');
-const { validateReview } = require('../middleware.js');
+const { validateReview, isLoggedIn } = require('../middleware.js');
 
 //post route for review
-router.post('/', validateReview, wrapAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let foundListing = await listing.findById(req.params.id);
     let newreview = new Review(req.body.Review);
+    newreview.author = req.user._id;~
     foundListing.reviews.push(newreview);
     await newreview.save();
     await foundListing.save();
